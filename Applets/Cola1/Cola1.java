8d59c3e3b0012cd2bdb76d4ff6ec32b101278f34
Wilmer Alzate <wilmeralzateroman@gmail.com>
2013-10-15 22:41:31 -0500

    udpate

---
+++ Applets/Cola1/Cola1.java
@@ -0,0 +1,15 @@
+import java.applet.*;
+import java.awt.*;
+
+public class Cola1 extends Applet{
+	
+	public void init(){
+		
+		}
+	
+	public void paint(Graphics g){
+		
+		g.drawString("Hola mundo",49,49);
+		
+		}
+}
